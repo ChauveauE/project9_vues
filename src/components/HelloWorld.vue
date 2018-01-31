1fd913d1df7becb38b7d4a471957ac7ee4825c2a
Elodie CHAUVEAU <elochauveau@hotmail.fr>
2018-01-31 21:44:07 +0100

    modif code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,141 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-
-    <!--<ul v-if="posts && posts.length">-->
-     <li v-for="post of posts">
-       <p><strong>{{post.title}}</strong></p>
-       <p>{{post.body}}</p>
-     </li>
-    <!--</ul>
-
-    <ul v-if="errors && errors.length"> -->
-      <li v-for="error of errors">
-        {{error.message}}
-      </li>
-    <!--</ul>-->
-
-    <input type="text" v-model="postBody" @change="postPost()"/>
-    <!--<ul v-if="errors && errors.length"> -->
-      <li v-for="error of errors">
-        {{error.message}}
-      </li>
-    <!--</ul> -->
-  </div>
-
     <ul>
       <li v-for="todo in todos" v-bind:key='todo.id'>
          {{ todo.id }} => {{ todo.label }}
       </li>
     </ul>
+  </div>
 </template>
 
 <script>
-  import axios from 'axios';
+import axios from 'axios'
 
 export default {
   name: 'HelloWorld',
 
-  data() {
-      return {
-        msg: 'Todo list',
-        postBody: '',
-        todos: []
-      }
-    },
+  data () {
+    return {
+      msg: 'Todo list',
+      postBody: '',
+      todos: []
+    }
+  },
 
-    created() {
-      axios.get('http://localhost:8000/api/todos')
+  created () {
+    axios.get('http://localhost:8000/api/todos')
       .then(response => {
         // JSON responses are automatically parsed.
         this.todos = response.data
       })
       .catch(e => {
-        //this.errors.push(e)
+        // this.errors.push(e)
         console.log(e)
       })
-    } 
+  }
 
-    // Pushes posts to the server when called.
+  // Pushes posts to the server when called.
 //      postPost() {
 //      axios.post(`http://jsonplaceholder.typicode.com/posts`, {
 //        body: this.postBody
@@ -147,9 +47,6 @@ export default {
 
 //    }
 }
-
-
-
 
 </script>
 
